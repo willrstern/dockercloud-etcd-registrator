f21d39932f14367fe341030e5c990bb92ac51edc
Will Stern <willrstern@gmail.com>
2016-05-27 11:11:38 -0500

    fixed require error

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 require('babel-register')
-var syncContainers = require('./src/docker-watch')
+var syncContainers = require('./src/docker-watch').default
 
 syncContainers()
 setInterval(syncContainers, process.env.REGISTER_INTERVAL || 5000)
